e6b0389f6defbe51a78118f592ec83fa177df038
Aliaksei Hirylovich <agirilovich@outlook.com>
2025-02-01 14:29:06 +0100

    remove creds

--- include/credentials.h
+++
@@ -1,9 +0,0 @@
-
-#define ssid_name "girilovich"
-#define ssid_password "1Dbiytdfz nhfdf 4"
-
-#define mqtt_server "ha.girilowicz.lan"
-#define mqtt_username "techrs"
-#define mqtt_password "cbI78mmmOn"
-
-#define MQTT_GENERAL_PREFIX "girilowicz"
